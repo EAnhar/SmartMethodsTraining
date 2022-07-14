6ada18ee17b0abd054e96dff702f2f28923e11f7
EAnhar <91504512+EAnhar@users.noreply.github.com>
2022-07-14 19:54:26 +0300

    format changes

--- Task1/SpeechToTextWebPage/S2T.js
+++ Task1/SpeechToTextWebPage/S2T.js
@@ -1,27 +1,42 @@
 start.addEventListener('click',function(){
-    var speech = true;
-    window.SpeechRecognition = window.webkitSpeechRecognition;
+  // initialization
+  var speech = true;
+  window.SpeechRecognition = window.webkitSpeechRecognition;
+  const recognition = new SpeechRecognition();
+  recognition.lang = 'ar'; 
+  recognition.continuous = true;
+  recognition.interimResults = true;
 
-    const recognition = new SpeechRecognition();
-    recognition.lang = 'ar';
-    recognition.continuous = true;
-    recognition.interimResults = true;
+  // these functions are for "listining" line display 
+  recognition.onstart = () => {
+    document.querySelector("#status").style.display = "block";
+  };
+  recognition.onerror = () => {
+    document.querySelector("#status").style.display = "none";
+    console.log("Speech Recognition Error");
+  };
+  recognition.onend = () => {
+    document.querySelector("#status").style.display = "none";
+    console.log("Speech Recognition Ended");
+  };
 
-    recognition.addEventListener('result', e => {
-        const transcript = Array.from(e.results)
-            .map(result => result[0])
-            .map(result => result.transcript)
-            .join('')
+  //function for transfer audio to text and display it 
+  recognition.addEventListener('result', e => {
+    const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('')
 
-            document.getElementById("final").innerHTML = transcript;
-            console.log(transcript);
-    });
+    document.getElementById("final").innerHTML = transcript;
+    console.log(transcript);
+  });
 
-    document.querySelector("#start").onclick = () => {
-        recognition.start();
-      };
-    document.querySelector("#stop").onclick = () => {
-        recognition.stop();
-      };
+  // start & stop button 
+  document.querySelector("#start").onclick = () => {
+      recognition.start();
+    };
+  document.querySelector("#stop").onclick = () => {
+      recognition.stop();
+    };
 
 });
